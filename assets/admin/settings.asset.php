792d3361f6db317fdb9e84622cfb8e7c27e4a541
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-08-03 00:31:57 +0600

    fix: add comprehensive CSS targeting for select controls

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7a9ea35d5faf02f95409');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9bef6f42d170314cae9e');
